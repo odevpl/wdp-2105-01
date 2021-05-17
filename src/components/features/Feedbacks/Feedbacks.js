8a531cef3335cc0a958c93e0bfe4e2a4090e3221
Robert Szyszka <robert.szyszka@outlook.com>
2021-05-18 01:53:23 +0200

    Add Feedback component

--- src/components/features/Feedbacks/Feedbacks.js
+++ src/components/features/Feedbacks/Feedbacks.js
@@ -1,28 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 import styles from './Feedbacks.module.scss';
 
 const Feedbacks = ({ feedbacks }) => (
   <div className={styles.root}>
     <div className='container pb-3'>
-      <div className='row '>
-        <div className={'col-12 justify-content-between '}>
-          {/* <button type="button" className="btn btn-dark rounded-0">
-            <a href='#'>
-              <FontAwesomeIcon icon={faAngleLeft} />
-            </a>
-          </button> */}
-          {feedbacks.map(feedback => (
-            <div key={feedback.id}>{feedback.text}</div>
-          ))}
-          {/* <button type="button" className="btn btn-dark rounded-0">
-            <a href='#'>
-              <FontAwesomeIcon icon={faAngleRight} />
-            </a>
-          </button> */}
+      <div className='row'>
+        <div className={'col-12'}>
+          <div className='row no-gutters align-items-end line'>
+            <div className={'col-12 col-md-auto ' + styles.heading}>
+              <h3>Client feedback</h3>
+            </div>
+            <div className={'col-auto ' + styles.dots}>
+              <ul>
+                <li>
+                  <a href='/#' className={styles.active}>
+                    page
+                  </a>
+                </li>
+                <li>
+                  <a href='/#'>page</a>
+                </li>
+                <li>
+                  <a href='/#'>page</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='row'>
+            <FontAwesomeIcon className={styles.icon} icon={faQuoteRight} />
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-10'>
+              <div
+                id='carouselIndicators'
+                className='carousel slide'
+                data-ride='carousel'
+              >
+                <div className='carousel-inner'>
+                  <div className='carousel-item active'>
+                    <p className='d-block w-100 text-center'>{feedbacks[0].text}</p>
+                  </div>
+                  <div className='carousel-item'>
+                    <p className='d-block w-100 text-center'>{feedbacks[1].text}</p>
+                  </div>
+                  <div className='carousel-item'>
+                    <p className='d-block w-100 text-center'>{feedbacks[2].text}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='row flex-column align-items-center'>
+            <div className='col-3'>
+              <div className='row flex-row align-items-center'>
+                <div className='col-4 flex-grow-1'>
+                  <img alt=''></img>
+                </div>
+                <div className='col-8 flex-column d-flex align-items-center'>
+                  <span className='col-12 name '>{feedbacks[0].name}</span>
+                  <span className='col-12'>{feedbacks[0].description}</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
