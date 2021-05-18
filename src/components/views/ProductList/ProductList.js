c1abcbecdb00ad5454bf82833a8193f3af8b0c20
Justyna Szewczyk <justyna0829@gmail.com>
2021-05-19 00:23:51 +0200

    Add small changes to the ProductList structure

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -5,12 +5,12 @@ import styles from './ProductList.module.scss';
 const ProductList = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div>BANNER</div>
+      <div className='col-12'>BANNER</div>
       <div className='row'>
         <div className='col-9'>PRODUCT LIST</div>
         <div className='col-3'>FILTERS</div>
       </div>
-      <div>BRANDS</div>
+      <div className='col-12'>BRANDS</div>
     </div>
   </div>
 );
