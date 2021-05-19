53471b5dc2bfbe2e316af12471ece8f78b13135c
Sebastian Sikora <ssikora80@gmail.com>
2021-05-19 19:01:23 +0200

    Fix Banner in ProductList

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
-import Banner from '../../features/Banner/Banner';
 
-const ProductPage = () => (
-  <div className={styles.root}>
-    <Banner />
-  </div>
-);
+const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
 
 // ProductPage.propTypes = {};
 
