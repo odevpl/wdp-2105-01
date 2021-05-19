d0b4c79d7341ff2c8b05b763c66b9345609a26d4
Sebastian Sikora <ssikora80@gmail.com>
2021-05-19 18:45:03 +0200

    Fix Banner in ProduckPage

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,33 +1,11 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
-import Button from '../../common/Button/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Banner from '../../features/Banner/Banner';
 
 const ProductPage = () => (
   <div className={styles.root}>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-12'>
-          <div className={styles.banner}>
-            <div className={styles.bannerContent}>
-              <h1>
-                BEDROOM <span>FURTNITURE</span>
-              </h1>
-              <h5>
-                ALWAYS <span>25%</span> OFF OR MORE
-              </h5>
-            </div>
-            <div className={styles.bannerNav}>
-              <Button className={styles.navBtn}>Home</Button>
-              <FontAwesomeIcon icon={faChevronRight} className={styles.icon} />
-              <Button className={styles.navBtn}>Furniture</Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Banner />
   </div>
 );
 
