6be074278b24b80a944d8809d4e3e83a62afb8c7
skwiryt <mzbprobny@gmail.com>
2021-05-20 16:52:26 +0200

    Add content and style to ProductPopup

--- src/components/features/ProductPopup/ProductPopup.js
+++ src/components/features/ProductPopup/ProductPopup.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ProductPopup.module.scss';
+import Stars from '../Stars/Stars';
+import Button from '../../common/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
 const ProductPopup = ({ product }) => {
+  const { id, customStars, stars, image, name, price } = product;
   return (
     <div className={styles.root}>
-      <div className={styles.content}>
-        <p>Hi, it&apos;s me - a popup.</p>
-        <p>And the name of the product is: {product.name}</p>
+      <div className='row no-gutters'>
+        <div className='col-6'>
+          <div className={styles.photo}>
+            <img className={styles.image} src={image} alt={name} />
+          </div>
+        </div>
+        <div className='col-6'>
+          <div className={styles.content}>
+            <div className={styles.content_upper}>
+              <Button>
+                <FontAwesomeIcon icon={faWindowClose}></FontAwesomeIcon>
+              </Button>
+            </div>
+            <div className={styles.content_bottom}>
+              <div className={styles.header}>
+                <h5>{product.name}</h5>
+                <Stars id={id} customStars={customStars} stars={stars} />
+              </div>
+              <div className={styles.line}></div>
+
+              <div className={styles.price}>
+                <div className={styles.price_old}>
+                  Old price:<span> $ 60</span>
+                </div>
+                <Button className={styles.priceBtn} noHover variant='small'>
+                  $ {price}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
