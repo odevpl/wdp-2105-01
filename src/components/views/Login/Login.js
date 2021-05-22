3dcab929e556bdc1ae4edf7840e93b5563443c94
Sebastian Sikora <ssikora80@gmail.com>
2021-05-22 16:10:02 +0200

    Fix Login subpage

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './Login.module.scss';
-import Button from '../../common/Button/Button';
+import { Link } from 'react-router-dom';
 
 const Login = () => (
   <div className={styles.root}>
@@ -9,21 +9,19 @@ const Login = () => (
       <div className={`col-10 col-sm-7 col-md-5 col-lg-3 ${styles.loginBox}`}>
         <div className={styles.contentBox}>
           <div className={styles.contentName}>Email</div>
-          <input type='email'></input>
+          <input type='email' required></input>
         </div>
         <div className={styles.contentBox}>
           <div className={styles.contentName}>Hasło</div>
-          <input type='password'></input>
+          <input type='password' required></input>
         </div>
         <div className={styles.contentBottom}>
           <p>Nie pamiętasz hasła?</p>
           <a href='#'>Przypomnij hasło</a>
         </div>
-        <a href='/' className={styles.button}>
-          <Button variant='small' className={styles.loginBtn}>
-            Zaloguj się
-          </Button>
-        </a>
+        <Link to={`/`} className={styles.buttonBox}>
+          <button className={styles.loginBtn}>Zaloguj się</button>
+        </Link>
       </div>
     </div>
   </div>
