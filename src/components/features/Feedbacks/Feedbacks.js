50a3afb10c3221ba34aead2f416d38113d0fdcc8
Robert Szyszka <robert.szyszka@outlook.com>
2021-05-27 21:09:38 +0200

    Add Client Feedback RWD

--- src/components/features/Feedbacks/Feedbacks.js
+++ src/components/features/Feedbacks/Feedbacks.js
@@ -10,8 +10,8 @@ const Feedbacks = ({ feedbacks }) => (
     <div className='container pb-3'>
       <div className='row'>
         <div className={'col-12'}>
-          <div className='row no-gutters align-items-end line'>
-            <div className={'col-12 col-md-auto ' + styles.heading}>
+          <div className={'row no-gutters align-items-end line ' + styles.dotspace}>
+            <div className={'col-11 col-sm-auto ' + styles.heading}>
               <h3>Client feedback</h3>
             </div>
           </div>
@@ -24,15 +24,16 @@ const Feedbacks = ({ feedbacks }) => (
               {feedbacks.map(feedback => (
                 <div div className='col-10 ' key={feedback.id}>
                   <p className='d-block w-100 text-center'>{feedback.text}</p>
-                  <div className='row flex-column align-items-center'>
-                    <div className='col-3'>
+                  <div className='row flex-column align-items-center d-flex justify-content-center'>
+                    <div className='col-12'>
                       <div className='row flex-row align-items-center'>
-                        <div className='col-4 flex-grow-1'>
+                        <div className='col-2 col-md-3 col-lg-4'></div>
+                        <div className='col-2 col-lg-1 flex-grow-1'>
                           <img alt=''></img>
                         </div>
-                        <div className='col-8 flex-column d-flex align-items-center'>
-                          <span className='col-12 name '>{feedback.name}</span>
-                          <span className='col-12'>{feedback.description}</span>
+                        <div className='col-8 col-md-6 flex-column d-flex align-items-center'>
+                          <span className='col-12 name'>{feedback.name}</span>
+                          <span className='col-12 '>{feedback.description}</span>
                         </div>
                       </div>
                     </div>
