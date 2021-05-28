a1f15ac0e2f3d2cd2fc235fdf664826440c2dcd9
Robert Szyszka <robert.szyszka@outlook.com>
2021-05-29 01:00:10 +0200

    Add coupon and totals RWD

--- src/components/views/Cart/Cart.js
+++ src/components/views/Cart/Cart.js
@@ -18,82 +18,89 @@ const Cart = props => {
           <p>Cart</p>
         </div>
       </div>
-      <div className={styles.cart}>
-        <table className='table'>
-          <thead className={styles.tableTop}>
-            <tr>
-              <th scope='col' className='align-middle text-center'>
-                PRODUCT
-              </th>
-              <th scope='col' className='align-middle text-left pl-3'>
-                PRICE
-              </th>
-              <th scope='col' className='align-middle text-left'>
-                QUANTITY
-              </th>
-              <th scope='col' className='align-middle text-center pl-3'>
-                TOTAL
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartProducts.map(product => (
-              <tr key={product.id}>
-                <th scope='row' className={styles.product}>
-                  <FontAwesomeIcon
-                    onClick={() => removeProduct(product.id)}
-                    className={styles.delete}
-                    icon={faTimes}
-                  />
-                  <div className={styles.image}>
-                    <img src={product.image} alt={product.name} />
-                  </div>
-                  {product.name}
+      <div className={styles.cartContainer}>
+        <div className={styles.cart}>
+          <table className='table'>
+            <thead className={styles.tableTop}>
+              <tr>
+                <th scope='col' className='align-middle text-center'>
+                  PRODUCT
                 </th>
-                <td className='align-middle'>{product.price}</td>
-                <td className='align-middle'>
-                  <div className={styles.quantity}>
-                    <Button className={styles.button1} variant='outline'>
-                      -
-                    </Button>
-                    <div className={styles.number}>
-                      <input></input>
-                    </div>
-                    <Button className={styles.button2} variant='outline'>
-                      +
-                    </Button>
-                  </div>
-                </td>
-                <td className='align-middle text-center pl-5'>$5.00</td>
+                <th scope='col' className='align-middle text-left pl-3'>
+                  PRICE
+                </th>
+                <th scope='col' className='align-middle text-left'>
+                  QUANTITY
+                </th>
+                <th scope='col' className='align-middle text-center pl-3'>
+                  TOTAL
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className={styles.coupon}>
-          <input type='text' placeholder='Coupon code'></input>
-          <Button className={styles.code} variant='small'>
-            APPLY COUPON
-          </Button>
-          <Button className={styles.update} variant='small'>
-            UPDATE CART
-          </Button>
+            </thead>
+            <tbody>
+              {cartProducts.map(product => (
+                <tr key={product.id}>
+                  <th scope='row' className={styles.product}>
+                    <FontAwesomeIcon
+                      onClick={() => removeProduct(product.id)}
+                      className={styles.delete}
+                      icon={faTimes}
+                    />
+                    <div className={styles.image}>
+                      <img src={product.image} alt={product.name} />
+                    </div>
+                    {product.name}
+                  </th>
+                  <td className='align-middle'>{product.price}</td>
+                  <td className='align-middle'>
+                    <div className={styles.quantity}>
+                      <Button className={styles.button1} variant='outline'>
+                        -
+                      </Button>
+                      <div className={styles.number}>
+                        <input></input>
+                      </div>
+                      <Button className={styles.button2} variant='outline'>
+                        +
+                      </Button>
+                    </div>
+                  </td>
+                  <td className='align-middle text-center pl-5'>$5.00</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className={styles.couponContainer}>
+            <div className={styles.couponSummary}>
+              <input type='text' placeholder='Coupon code'></input>
+              <Button className={styles.code} variant='small'>
+                APPLY COUPON
+              </Button>
+            </div>
+            <Button className={styles.update} variant='small'>
+              UPDATE CART
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className={styles.summary}>
-        <h3>Cart totals</h3>
-        <div className={styles.subtotal}>
-          <h5>Subtotal</h5>
-          <p>$92.00</p>
+        <div className={styles.summaryContainer}>
+          <div className={styles.summary}>
+            <h3>Cart totals</h3>
+            <div className={styles.subtotal}>
+              <h5>Subtotal</h5>
+              <p>$92.00</p>
+            </div>
+            <div className={styles.total}>
+              <h5>Total</h5>
+              <p>$92.00</p>
+            </div>
+            <Link to={`/`} className={styles.checkout}>
+              <Button onClick={() => clearCart()} className={styles.checkoutButton}>
+                PROCEED TO CHECKOUT
+              </Button>
+            </Link>
+          </div>
         </div>
-        <div className={styles.total}>
-          <h5>Total</h5>
-          <p>$92.00</p>
-        </div>
-        <Link to={`/`} className={styles.checkout}>
-          <Button onClick={() => clearCart()} className={styles.checkoutButton}>
-            PROCEED TO CHECKOUT
-          </Button>
-        </Link>
       </div>
     </div>
   );
