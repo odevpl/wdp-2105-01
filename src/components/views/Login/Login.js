754f7fd1155f7a61753d9c8b0db12287227cc04c
PatrykBula <patryk.bula@performgroup.com>
2021-05-31 09:27:28 +0200

    Fix for Login

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import styles from './Login.module.scss';
-
-function validate() {
-  let email, passwd, text;
-
-  email = document.getElementById('email').value;
-  passwd = document.getElementById('passwd').value;
-
-  if (email !== 'admin') {
-    text = 'Invalid login';
-  } else if (passwd !== 'pass') {
-    text = 'Invalid passwdord';
-  } else {
-    text = 'Everything OK';
-  }
-
-  document.getElementById('alert').innerHTML = text;
-}
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const validate = () => {
+    let email, passwd, text;
+
+    email = document.getElementById('email').value;
+    passwd = document.getElementById('passwd').value;
+
+    if (email !== 'admin') {
+      text = 'Invalid login';
+    } else if (passwd !== 'pass') {
+      text = 'Invalid passwdord';
+    } else {
+      text = 'Everything OK';
+    }
+
+    document.getElementById('alert').innerHTML = text;
+  };
+
   const formValidation = event => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
@@ -54,31 +55,37 @@ const Login = () => {
 
   return (
     <div className={styles.root}>
-        <div className={styles.loginContainer}>
-            <div className={`col-11 ${styles.loginBox}`}>
-                <div className={styles.contentBox}>
-                    <div className={styles.contentName}>Login</div>
-                    <input type='email' id='email' onKeyUp={clearMessage} required></input>
-                </div>
-                <div className={styles.contentBox}>
-                    <div className={styles.contentName}>Password</div>
-                    <input type='password' id='password' onKeyUp={clearMessage} required></input>
-                </div>
-                <div className={styles.contentBottom}>
-                    <p>Forgot password?</p>
-                    <a href='#'>Remind password</a>
-                </div>
-                <div id='message'> </div>
-                <Link onClick={formValidation} to={`/`} className={styles.buttonBox}>
-                <button className={styles.loginBtn}>Log in</button>
-                </Link>
-            </div>
-            <p id='alert'></p>
-            <button className={styles.loginBtn} onClick={()=>validate()}>
-                Zaloguj się
-            </button>
+      <div className={styles.loginContainer}>
+        <div className={`col-11 ${styles.loginBox}`}>
+          <div className={styles.contentBox}>
+            <div className={styles.contentName}>Login</div>
+            <input type='email' id='email' onKeyUp={clearMessage} required></input>
+          </div>
+          <div className={styles.contentBox}>
+            <div className={styles.contentName}>Password</div>
+            <input
+              type='password'
+              id='password'
+              onKeyUp={clearMessage}
+              required
+            ></input>
+          </div>
+          <div className={styles.contentBottom}>
+            <p>Forgot password?</p>
+            <a href='#'>Remind password</a>
+          </div>
+          <div id='message'> </div>
+          <Link onClick={formValidation} to={`/`} className={styles.buttonBox}>
+            <button className={styles.loginBtn}>Log in</button>
+          </Link>
         </div>
+        <p id='alert'></p>
+        <button className={styles.loginBtn} onClick={() => validate()}>
+          Zaloguj się
+        </button>
+      </div>
     </div>
-);
+  );
+};
 
 export default Login;
