ff7d142a8254676eadcd5f9ce9ff876861e59bdf
Justyna Szewczyk <justyna0829@gmail.com>
2021-06-01 12:05:17 +0200

    Finish price filter

--- src/components/features/PriceFilter/PriceFilter.js
+++ src/components/features/PriceFilter/PriceFilter.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import styles from './Filters.module.scss';
+import styles from './PriceFilter.module.scss';
 import Button from '../../common/Button/Button';
 import 'rc-slider/assets/index.css';
 import 'rc-tooltip/assets/bootstrap.css';
-
 
 const Slider = require('rc-slider');
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 
-const Filters = () => (
+const PriceFilter = () => (
   <div className={styles.root}>
     <div className={styles.header}>
       <h5>Filter by price</h5>
@@ -18,34 +17,36 @@ const Filters = () => (
       <Range className={styles.range}
         min={0}
         max={1000}
-        defaultValue={[200, 600]}
+        defaultValue={[135, 500]}
         allowCross={false}
-        tipFormatter={value => `${value}$`}
-
+        tipFormatter={value => `${value} $`}
         railStyle={{ backgroundColor: '#fff', border: 'solid 1px #d4d4d4', height: '8px', borderRadius: '2px'}}
-
         handleStyle={{
           borderColor: '#d58e32',
           borderWidth: '3px',
           height: 20,
           width: 20,
           backgroundColor: '#fff',
+          boxShadow: 'none',
         }}
-
         trackStyle={[{
           backgroundColor: '#2a2a2a',
           height: '8px',
         }]}
-
       />
-
     </div>
-
-    <Button className={styles.button} variant='small'>
-      Filter
-    </Button>
-
+    <div className={styles.options}>
+      <Button className={styles.button} variant='small'>
+        Filter
+      </Button>
+      <p>
+        $
+        <input type='text' placeholder='135'></input>
+        -  $
+        <input type='text' placeholder='500'></input>
+      </p>
+    </div>
   </div>
 );
 
-export default Filters;
+export default PriceFilter;
